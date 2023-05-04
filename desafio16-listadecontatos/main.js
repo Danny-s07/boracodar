84de5de22a4cff27b848c9fc22e3bb5cfb763364
igorMSoares <igorms.dev@gmail.com>
2023-05-04 15:36:38 -0300

    modifica html e css para tornar o layout responsivo
    também torna independentes contatos que começam com a mesma letra,
    de tal forma que se buscarmos por 'Br', 'Bea' será ocultada e apenas 'Brenda' irá aparecer

--- desafio16-listadecontatos/main.js
+++ desafio16-listadecontatos/main.js
@@ -1,7 +1,7 @@
 //tentando criar um filtro na lista de contatos
 
 const filterElement = document.querySelector('header input')
-const cards = document.querySelectorAll('.cards')
+const contactsList = document.querySelectorAll('.person')
 
 filterElement.addEventListener('input', filterCards)
 
@@ -9,20 +9,32 @@ function filterCards() {
   const filterValue = filterElement.value
 
   if (filterValue !== '') {
-    for (let card of cards) {
-      const title = card.querySelector('h3').textContent.toLocaleLowerCase()
+    for (const person of contactsList) {
+      const title = person.querySelector('h3').textContent.toLocaleLowerCase()
       const filterText = filterValue.toLowerCase()
 
-      !title.includes(filterText)
-        ? card.parentElement.classList.add('display-none')
-        : card.parentElement.classList.remove('display-none')
+      if (!title.includes(filterText)) {
+        person.classList.add('display-none')
+
+        // Verifica se todas as pessoas da lista foram escondidas pelo filtro
+        if (
+            [... person.parentElement.querySelectorAll('.person')]
+            .every((child) => child.classList.contains('display-none'))
+            ) {
+              // Se todas as pessoas da lista estão ocultas,
+              // então oculta toda a lista incluindo a letra
+              person.parentElement.parentElement.classList.add('display-none')
+            }
+      } else {
+        person.classList.remove('display-none')
+      }
     }
   } else {
     const hiddenCardsList = document.querySelectorAll(
-      '.list-wrapper.display-none'
+      '.display-none'
     )
 
-    for (let hiddenCard of hiddenCardsList) {
+    for (const hiddenCard of hiddenCardsList) {
       hiddenCard.classList.remove('display-none')
     }
   }
