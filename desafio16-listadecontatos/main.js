a607ce0af2371323d8d8a6ae7551851f89ee634c
igorMSoares <igorms.dev@gmail.com>
2023-05-04 13:37:36 -0300

    refatora logica do filtro de contatos

--- desafio16-listadecontatos/main.js
+++ desafio16-listadecontatos/main.js
@@ -1,32 +1,29 @@
 //tentando criar um filtro na lista de contatos
 
-
 const filterElement = document.querySelector('header input')
-const cards = document.querySelectorAll('.cards li')
+const cards = document.querySelectorAll('.cards')
 
 filterElement.addEventListener('input', filterCards)
 
-function filterCards()
-{
-    if(filterElement.value !== ''){
-        for(let card of cards){
-            let title = card.querySelector('h3')
-            title = title.textContent.toLowerCase()
-            let filterText = filterElement.value.toLowerCase()
-            if (!title.includes(filterText)) {
-                card.style.display = "none"
-                // document.getElementById(".letter").style.display = "none"
-                
-            }
-            else{
-                card.style.display = "block"
-            }
-        }
-    }
-    else{
-        for (let card of cards) {
-            card.style.display = "block"
-        }
-    }
-}
+function filterCards() {
+  const filterValue = filterElement.value
 
+  if (filterValue !== '') {
+    for (let card of cards) {
+      const title = card.querySelector('h3').textContent.toLocaleLowerCase()
+      const filterText = filterValue.toLowerCase()
+
+      !title.includes(filterText)
+        ? card.parentElement.classList.add('display-none')
+        : card.parentElement.classList.remove('display-none')
+    }
+  } else {
+    const hiddenCardsList = document.querySelectorAll(
+      '.list-wrapper.display-none'
+    )
+
+    for (let hiddenCard of hiddenCardsList) {
+      hiddenCard.classList.remove('display-none')
+    }
+  }
+}
